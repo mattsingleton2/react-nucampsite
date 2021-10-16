293081c388dbee3c6998cc03487cefed37c12d5b
Matt Singleton <matt.singleton2@gmail.com>
2021-10-16 06:07:05 -0500

    React router parameters and directory scaffolding. Edited the onclick event to do a react router to that ID.

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,12 +1,15 @@
 import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
+import { Link } from "react-router-dom";
 
-function RenderDirectoryItem({ campsite, onClick }) {
+function RenderDirectoryItem({ campsite }) {
   return (
     <Card>
-      <CardImg width="100%" src={campsite.image} alt={campsite.name} />
-      <CardImgOverlay>
-        <CardTitle>{campsite.name}</CardTitle>
-      </CardImgOverlay>
+      <Link to={`/directory/${campsite.id}`}>
+        <CardImg width="100%" src={campsite.image} alt={campsite.name} />
+        <CardImgOverlay>
+          <CardTitle>{campsite.name}</CardTitle>
+        </CardImgOverlay>
+      </Link>
     </Card>
   );
 }
