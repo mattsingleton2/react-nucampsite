2e0c299f5b6b6ba96d5f1b0a6c5b24de680ced63
Matt Singleton <matt.singleton2@gmail.com>
2021-10-25 13:09:17 -0500

    I messed up and was importing from the wrong locations. Fixed.

--- src/redux/promotions.js
+++ src/redux/promotions.js
@@ -1,4 +1,4 @@
-import { PROMOTIONS } from "../shared/campsites";
+import { PROMOTIONS } from "../shared/promotions";
 
 export const Promotions = (state = PROMOTIONS, action) => {
   switch (action.type) {
